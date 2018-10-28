b1b68143ca408bf39f3ffae074019dd0bc719f0f
Linus Probert <linus.probert@gmail.com>
2018-10-28 16:08:51 +0100

    Cleaned up the test code slightly

--- test/TestECS.cpp
+++ test/TestECS.cpp
@@ -11,30 +11,18 @@ TEST_CASE("Test Entity/Component", "[entity]")
 	TestComponent *c2 = new TestComponent();
 	e.addComponent(c1);
 	e.addComponent(c2);
-}
 
-TEST_CASE("Test messageing", "[entity][message][component]")
-{
-	TestEntity e;
-	TestComponent *c1 = new TestComponent();
-	TestComponent *c2 = new TestComponent();
-	e.addComponent(c1);
-	e.addComponent(c2);
+	SECTION("Test messaging", "[message][component]")
+	{
+		e.transmit(TestMessage(UPDATE, "A message"));
+		REQUIRE(c1->received == "A message");
+		REQUIRE(c2->received == "A message");
+	}
 
-	e.transmit(TestMessage(UPDATE, "A message"));
-	REQUIRE(c1->received == "A message");
-	REQUIRE(c2->received == "A message");
-}
-
-TEST_CASE("Test update", "[entity][update][component]")
-{
-	TestEntity e;
-	TestComponent *c1 = new TestComponent();
-	TestComponent *c2 = new TestComponent();
-	e.addComponent(c1);
-	e.addComponent(c2);
-
-	e.update(TestUpdate("A message"));
-	REQUIRE(c1->updateMsg == "A message");
-	REQUIRE(c2->updateMsg == "A message");
+	SECTION("Test update", "[update][component]")
+	{
+		e.update(TestUpdate("An update"));
+		REQUIRE(c1->updateMsg == "An update");
+		REQUIRE(c2->updateMsg == "An update");
+	}
 }
